e56f73ccf8c2b81146a74247d99f8f090e33584e
Manideep Alimi <manideep.a@comakeit.com>
2022-07-07 13:00:11 +0530

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventHandleComponent } from './event-handle/event-handle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventHandleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/event-handle/event-handle.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event-handle',
+  templateUrl: './event-handle.component.html',
+  styleUrls: ['./event-handle.component.css']
+})
+export class EventHandleComponent implements OnInit {
+  name:string='';
+  error:Boolean=false;
+  selectedvalue:string='India'
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  OnChangeName(e:any){
+    console.log(e)
+    if(e.trim().length==0)
+      this.error=true;
+    else 
+      this.error=false;
+    this.name=e;
+  }
+
+  submit(){
+    if(!this.error)
+    alert("Form submitted!")
+  }
+}
+
